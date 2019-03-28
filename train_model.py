606cbf59b1386630cee80b38a42b78eeb5efc751
Wang Gengyu <posuer000@gmail.com>
2019-03-28 16:59:25 +0900

    minor fix

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 from NLImodel.models import NLImodelClass
-'''
-model = NLImodel(is_train=True)
+
+model = NLImodelClass(is_train=True)
 
 '''
 #Load traind model and predict
@@ -8,4 +8,4 @@ model = NLImodelClass(is_train=False)
 print(model.train_X[0:2], model.train_Y[0:2], model.train_Z[0:2])
 result = model.predict(model.train_X[0:2], model.train_Y[0:2]) 
 print(result)
-
+'''
